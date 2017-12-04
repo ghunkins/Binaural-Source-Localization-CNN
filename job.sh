03824c1c81487567791339f2460e38ce8a72f8d6
Greg <ghunkins@users.noreply.github.com>
2017-12-03 21:25:49 -0500

    update job.sh

--- job.sh
+++ job.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=binaural
 #SBATCH --output=binaural.txt
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --ntasks-per-node=1
 #SBATCH --time=8:00:00
 #SBATCH --mem-per-cpu=50GB
 #SBATCH --gres=gpu:2
