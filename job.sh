6993e3f687ae82a567b2813b0f681caffd06434d
Greg <ghunkins@users.noreply.github.com>
2017-12-03 21:34:07 -0500

    more job.sh

--- job.sh
+++ job.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+#SBATCH -p gpu
+#SBATCH --mem-per-cpu=10gb
+#SBATCH -t 0-04:00:00
 #SBATCH --job-name=binaural
 #SBATCH --output=binaural.txt
+#SBATCH -e error.txt
+#SBATCH --mail-type=begin
+#SBATCH --mail-user=ghunkins@u.rochester.edu
 #SBATCH --time=8:00:00
 #SBATCH --gres=gpu:1
 
+source activate keras
 python neuralnet.py
