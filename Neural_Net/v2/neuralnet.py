c056327bd3316a99b22986219c8c761f47077707
Greg <ghunkins@users.noreply.github.com>
2017-12-05 11:38:47 -0500

    input shape fix

--- Neural_Net/v2/neuralnet.py
+++ Neural_Net/v2/neuralnet.py
@@ -51,7 +51,7 @@ validation_generator = DataGenerator(**params).generate(Test_IDs)
 
 # Design model
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(804, 48, 1)))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(804, 47, 1)))
 model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
 model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
 model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
