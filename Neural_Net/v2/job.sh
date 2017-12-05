150367c5810f1b317b09745b2c9250996ce554bd
Greg <ghunkins@users.noreply.github.com>
2017-12-05 13:22:02 -0500

    job.sh test

--- Neural_Net/v2/job.sh
+++ Neural_Net/v2/job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -p gpu
 #SBATCH -N 10                    
-#SBATCH --mem=125gb            
+#SBATCH --mem=25gb            
 #SBATCH --ntasks-per-node=2
 #SBATCH -t 0-04:00:00
-#SBATCH --job-name=final
-#SBATCH --output=final.txt
+#SBATCH --job-name=binaural
+#SBATCH --output=binaural.txt
 #SBATCH -e error.txt
 #SBATCH --mail-type=begin
 #SBATCH --mail-user=ghunkins@u.rochester.edu
