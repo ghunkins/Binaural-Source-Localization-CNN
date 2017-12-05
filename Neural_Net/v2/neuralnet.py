aadfab7ea3e90d2eec2be238ad304daebf86a31b
Greg <ghunkins@users.noreply.github.com>
2017-12-05 11:17:34 -0500

    neural net II

---
+++ Neural_Net/v2/neuralnet.py
@@ -0,0 +1,77 @@
+"""
+Main Neural Network Pipeline.
+"""
+#-------------------------- set gpu using tf ---------------------------#
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
+
+#-------------------  start importing keras module ---------------------#
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, Conv1D, GlobalAveragePooling2D
+from datagenerator import DataGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import itertools
+import os
+
+# Parameters
+
+if os.path.abspath('~') == '/Users/ghunk/~':
+	data_root = "/Users/ghunk/Desktop/GRADUATE/CSC_464/Final_Project/Dataset/stft/"
+else:
+	data_root = "/scratch/ghunkins/stft/"
+
+elevations = [-45, -30, -15, 0, 15, 30, 45]
+azimuths = [15*x for x in range(24)]
+el_az = list(itertools.product(elevations, azimuths))
+classes = [str(x) + '_' + str(y) for x, y in el_az]
+encoder = LabelEncoder()
+encoder.fit(classes)
+
+params = {'batch_size': 32,
+		  'Y_encoder': encoder,
+          'shuffle': True}
+
+LIMIT = 10000
+RANDOM_STATE = 3
+
+# Datasets
+IDs = os.listdir(data_root)[:LIMIT]
+
+Train_IDs, Test_IDs, _, _, = train_test_split(IDs, np.arange(len(IDs)), test_size=0.2, random_state=RANDOM_STATE)
+
+# Generators
+training_generator = DataGenerator(**params).generate(Train_IDs)
+validation_generator = DataGenerator(**params).generate(Test_IDs)
+
+# Design model
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(804, 48, 1)))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
+model.add(GlobalAveragePooling2D(data_format='channels_last'))
+
+model.add(Dense(168, activation='relu'))
+model.add(Dense(168, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+# Train model on dataset
+
+model.fit_generator(generator = training_generator,
+					steps_per_epoch = len(Train_IDs)//params['batch_size'],
+                    nb_epoch = 10, 
+                    validation_data = validation_generator,
+                    validation_steps = len(Test_IDs)//params['batch_size'],
+                    verbose=2)
+
+#model.save_weights("/Users/ghunk/Desktop/GRADUATE/CSC_464/Final_Project/weights.h5py")
+model.save_weights("/home/ghunkins/weights.h5py")
--- job.sh
+++ job.sh
@@ -4,8 +4,8 @@
 #SBATCH --mem=125gb            
 #SBATCH --ntasks-per-node=2
 #SBATCH -t 0-04:00:00
-#SBATCH --job-name=binaural
-#SBATCH --output=binaural.txt
+#SBATCH --job-name=final
+#SBATCH --output=final.txt
 #SBATCH -e error.txt
 #SBATCH --mail-type=begin
 #SBATCH --mail-user=ghunkins@u.rochester.edu
