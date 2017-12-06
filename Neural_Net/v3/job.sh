bc75d4469149997dacb6e990382756d5e18d24d6
Greg <ghunkins@users.noreply.github.com>
2017-12-05 20:12:19 -0500

    update model

---
+++ Neural_Net/v3/job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH -N 1                    
+#SBATCH --mem=25gb            
+#SBATCH --ntasks-per-node=1
+#SBATCH -t 0-04:00:00
+#SBATCH --job-name=binaural
+#SBATCH --output=binaural.txt
+#SBATCH -e error.txt
+#SBATCH --mail-type=begin
+#SBATCH --mail-user=ghunkins@u.rochester.edu
+#SBATCH --gres=gpu:1
+
+source activate keras
+python neuralnet.py
