3478bb490ef124fb9fc1a6ec7f8e2a391ad0057f
Greg <ghunkins@users.noreply.github.com>
2017-12-05 21:09:44 -0500

    job.sh changes

--- Neural_Net/v3/job.sh
+++ Neural_Net/v3/job.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 #SBATCH -p gpu
-#SBATCH -N 1                    
-#SBATCH --mem=25gb            
-#SBATCH --ntasks-per-node=1
-#SBATCH -t 0-04:00:00
+#SBATCH -t 0-08:00:00
 #SBATCH --job-name=binaural
-#SBATCH --output=binaural.txt
-#SBATCH -e error.txt
-#SBATCH --mail-type=begin
-#SBATCH --mail-user=ghunkins@u.rochester.edu
-#SBATCH --gres=gpu:4
+#SBATCH --output=binaural_%j.txt
+#SBATCH -e error_%j.txt
+#SBATCH --gres=gpu:2
 
 source activate keras
 python neuralnet.py
