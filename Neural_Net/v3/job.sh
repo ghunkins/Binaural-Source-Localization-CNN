1592fb09208bb2cf068672ffb2d89043ab6513b6
Greg <ghunkins@users.noreply.github.com>
2017-12-06 13:24:23 -0500

    200000

--- Neural_Net/v3/job.sh
+++ Neural_Net/v3/job.sh
@@ -2,8 +2,8 @@
 #SBATCH -p gpu
 #SBATCH -t 0-08:00:00
 #SBATCH --job-name=binaural
-#SBATCH --output=binaural_%j.txt
-#SBATCH -e error_%j.txt
+#SBATCH --output=binaural_200000_%j.txt
+#SBATCH -e error_200000_%j.txt
 #SBATCH --gres=gpu:2
 
 source activate keras
