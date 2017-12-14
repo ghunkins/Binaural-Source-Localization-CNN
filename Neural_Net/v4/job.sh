d526350fd21fb0d3e436ca493b8d48ca6ff66dc7
Greg <ghunkins@users.noreply.github.com>
2017-12-14 07:33:01 -0500

    job.sh

--- Neural_Net/v4/job.sh
+++ Neural_Net/v4/job.sh
@@ -2,9 +2,9 @@
 #SBATCH -p gpu
 #SBATCH -t 0-04:00:00
 #SBATCH --mem=10GB
-#SBATCH --job-name=10db
-#SBATCH --output=10db_%j.txt
-#SBATCH -e 10db_%j.txt
+#SBATCH --job-name=20db
+#SBATCH --output=20db_%j.txt
+#SBATCH -e 20db_%j.txt
 #SBATCH --gres=gpu:2
 
 source activate keras
